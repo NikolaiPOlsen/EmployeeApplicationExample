6fc59262788cfce0693f76dc664b825fbd5cbb76
Nikolai <nikolai.pedersen.olsen@worksoft.no>
2026-09-10 11:16:42 +0200

    Added Button component to home page imports

--- src/app/Pages/home-page/home-page.ts
+++ src/app/Pages/home-page/home-page.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { Button } from "../../AppComponent/button/button";
 
 @Component({
-  imports: [],
+  imports: [Button],
   selector: 'app-home-page',
   styleUrl: './home-page.scss',
   templateUrl: './home-page.html',
