2ff5fadf5e83014918cd93771380f3f050bb8d47
Nikolai <nikolai.pedersen.olsen@worksoft.no>
2026-09-10 11:11:46 +0200

    Refactored to include navbar import

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './AppComponent/navbar/navbar';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
