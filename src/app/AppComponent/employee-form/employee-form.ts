077894727ab9f9d0c4fbb0ff4607f8c49d52f4db
Nikolai <nikolai.pedersen.olsen@worksoft.no>
2026-09-10 13:47:23 +0200

    Refactored FormGroup

--- src/app/AppComponent/employee-form/employee-form.ts
+++ src/app/AppComponent/employee-form/employee-form.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { EmployeeDataService } from '../../employee.data.service';
 import { FormsModule, FormControl, ReactiveFormsModule, FormGroup, Validators, RequiredValidator } from '@angular/forms';
 import { EmployeeData } from '../../employee-data';
@@ -14,15 +14,16 @@ import { Subscription } from 'rxjs';
   styleUrl: './employee-form.scss',
   templateUrl: './employee-form.html',
 })
-export class EmployeeForm implements OnInit{
+export class EmployeeForm implements OnInit, OnChanges{
   private employeeSub!: Subscription;
 
   employeeForm = new FormGroup({
-    name: new FormControl('', Validators.required),
-    email: new FormControl('', Validators.required),
-    role: new FormControl('', Validators.required),
-    department: new FormControl('', Validators.required),
-    status: new FormControl('Active'),
+    id: new FormControl(0, {nonNullable:true, validators: [Validators.required]}),
+    name: new FormControl('', {nonNullable:true, validators: [Validators.required]}),
+    email: new FormControl('', {nonNullable:true, validators: [Validators.required]}),
+    role: new FormControl('', {nonNullable:true, validators: [Validators.required]}),
+    department: new FormControl('', {nonNullable:true, validators: [Validators.required]}),
+    status: new FormControl('Active', {nonNullable:true}),
   }); 
 
   employee: EmployeeData = {
@@ -34,23 +35,34 @@ export class EmployeeForm implements OnInit{
     status: 'Active',
   };
 
+  @Input() employeeToEdit!: EmployeeData | null;
+
   @Output() getEmployeeList = new EventEmitter();
   buttonName = "Submit";
 
   constructor(
     public employeeDataService: EmployeeDataService) {}
 
-  onSubmit() {
-    this.employeeDataService.addEmployee(this.employee);
 
-    this.employee = {
-      id: 0,
-      name: '',
-      email: '',
-      role: '',
-      department: '',
-      status: 'Active',
+
+  onSubmit() {
+    console.log(this.employeeForm.value)
+    console.log(this.employeeForm.valid)
+    console.log(this.employeeForm.reset())
+
+    const formValue = this.employeeForm.value
+    console.log(formValue.name)
+    formValue.id = this.employeeDataService.generateEmployeeId();
+
+    const employee: EmployeeData = {
+      id: this.employeeDataService.generateEmployeeId(),
+      name: formValue.name,
+      email: formValue.email,
+      role: formValue.role,
+      department: formValue.department,
+      status: formValue.status,
     };
+    this.employeeDataService.addEmployee(employee);
     const employeeList = this.employeeDataService.getEmployeeListData()
     const list = this.employeeDataService.returnList();
     this.getEmployeeList.emit(list);
@@ -61,15 +73,9 @@ export class EmployeeForm implements OnInit{
     this.onSubmit();
   }
 
-  updateFormWithEmployeeData() {
-    this.employee = {
-      id: this.employee.id,
-      name: this.employee.name,
-      email: this.employee.email,
-      role: this.employee.role,
-      department: this.employee.department,
-      status: this.employee.status
-    }
+  updateFormWithEmployeeData(selectedEmployee: EmployeeData) {
+    this.employeeToEdit 
+    this.employee = {... selectedEmployee};
     this.employeeForm.patchValue(
       {
         name: this.employee.name,
@@ -82,7 +88,11 @@ export class EmployeeForm implements OnInit{
   }
 
   ngOnInit(): void {
+    this.employeeForm.get("name")?.valueChanges.subscribe((nameValue) => {console.log(nameValue)});
     //this.employeeSub = this.employeeDataService.getEmployeeUpdateData().subscribe((selectedEmployee: EmployeeData | null) => {
     //});
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
   }
 }
