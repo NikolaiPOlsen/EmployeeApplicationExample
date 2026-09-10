eef22a2cb51fa61cf4ce980672d1d9ef06e4a10e
Nikolai <nikolai.pedersen.olsen@worksoft.no>
2026-09-10 11:15:42 +0200

    Creatde Navbar component with routing and button integration

---
+++ src/app/AppComponent/navbar/navbar.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Button } from "../button/button";
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-navbar',
+  imports: [Button, RouterLink],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.scss',
+})
+export class Navbar {
+}
