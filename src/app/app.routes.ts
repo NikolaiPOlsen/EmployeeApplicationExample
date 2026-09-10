75df0a6e52bd347c4e80b7167e49b02c3b51001c
Nikolai <nikolai.pedersen.olsen@worksoft.no>
2026-09-10 11:11:30 +0200

    refactored to include new page routes, with both lazy and eager loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,9 @@ import { HomePage } from './Pages/home-page/home-page';
 import { EmployeePage } from './Pages/employee-page/employee-page';
 
 export const routes: Routes = [
-  { path: '', component: EmployeePage },
-];
+  { path: '', component: HomePage},
+  { path: 'about', loadComponent: () => import('./Pages/about-page/about-page').then(m => m.AboutPage) },
+  { path: 'contact', loadComponent: () => import('./Pages/contact-page/contact-page').then(m => m.ContactPage) },
+  { path: 'employees', component: EmployeePage},
+    ];
+
