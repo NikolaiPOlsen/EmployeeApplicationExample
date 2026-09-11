f548a63b1800c609c0f8133be6160775368b95fd
Nikolai <nikolai.pedersen.olsen@worksoft.no>
2026-09-11 11:06:24 +0200

    Imported routerlink

--- src/app/Pages/home-page/home-page.ts
+++ src/app/Pages/home-page/home-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { Button } from "../../AppComponent/button/button";
+import { RouterLink } from '@angular/router';
 
 @Component({
-  imports: [Button],
+  imports: [Button, RouterLink],
   selector: 'app-home-page',
   styleUrl: './home-page.scss',
   templateUrl: './home-page.html',
