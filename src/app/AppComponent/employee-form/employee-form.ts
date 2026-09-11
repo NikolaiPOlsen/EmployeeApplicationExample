526d1da7ba3aae77c0c309da37d8eca3c5b50ed4
Nikolai <nikolai.pedersen.olsen@worksoft.no>
2026-09-11 11:04:36 +0200

    Removed lagacy code

--- src/app/AppComponent/employee-form/employee-form.ts
+++ src/app/AppComponent/employee-form/employee-form.ts
@@ -1,26 +1,24 @@
 import { Component, OnInit, Output, EventEmitter, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { EmployeeDataService } from '../../employee.data.service';
-import { FormsModule, FormControl, ReactiveFormsModule, FormGroup, Validators, RequiredValidator } from '@angular/forms';
+import { FormControl, ReactiveFormsModule, FormGroup, Validators } from '@angular/forms';
 import { EmployeeData } from '../../employee-data';
 import { Button } from "../button/button";
-import { Subscription } from 'rxjs';
-
-
-
 
 @Component({
-  imports: [FormsModule, Button, ReactiveFormsModule],
+  imports: [Button, ReactiveFormsModule],
   selector: 'app-employee-form',
   styleUrl: './employee-form.scss',
   templateUrl: './employee-form.html',
 })
 export class EmployeeForm implements OnInit, OnChanges{
-  private employeeSub!: Subscription;
+  updateButtonEnable: boolean = false;
+  submitButtonDisable: boolean = false;
+  //private employeeSub!: Subscription;
 
   employeeForm = new FormGroup({
     id: new FormControl(0, {nonNullable:true, validators: [Validators.required]}),
     name: new FormControl('', {nonNullable:true, validators: [Validators.required]}),
-    email: new FormControl('', {nonNullable:true, validators: [Validators.required]}),
+    email: new FormControl('', {nonNullable:true, validators: [Validators.required, Validators.email]}),
     role: new FormControl('', {nonNullable:true, validators: [Validators.required]}),
     department: new FormControl('', {nonNullable:true, validators: [Validators.required]}),
     status: new FormControl('Active', {nonNullable:true}),
@@ -38,42 +36,40 @@ export class EmployeeForm implements OnInit, OnChanges{
   @Input() employeeToEdit!: EmployeeData | null;
 
   @Output() getEmployeeList = new EventEmitter();
-  buttonName = "Submit";
+  @Output() cancelled = new EventEmitter<void>();
+  //buttonName = "Submit";
 
   constructor(
     public employeeDataService: EmployeeDataService) {}
 
-
-
   onSubmit() {
-    console.log(this.employeeForm.value)
-    console.log(this.employeeForm.valid)
+      const formValue = this.employeeForm.getRawValue();
 
-    const formValue = this.employeeForm.getRawValue();
-    console.log(formValue.name)
-    formValue.id = this.employeeDataService.generateEmployeeId();
+      formValue.id = this.employeeDataService.generateEmployeeId();
 
-    // const employee: EmployeeData = {
-    //   id: this.employeeDataService.generateEmployeeId(),
-    //   name: formValue.name,
-    //   email: formValue.email,
-    //   role: formValue.role,
-    //   department: formValue.department,
-    //   status: formValue.status,
-    // };
-    this.employeeDataService.addEmployee(formValue);
-    const employeeList = this.employeeDataService.getEmployeeListData()
-    const list = this.employeeDataService.returnList();
-    this.getEmployeeList.emit(list);
-    this.employeeForm.reset();
+      this.employeeDataService.addEmployee(formValue);
+      const employeeList = this.employeeDataService.getEmployeeListData()
+      const list = this.employeeDataService.returnList();
+      this.getEmployeeList.emit(list);
 
-  }
+      this.employeeForm.reset();
+    }
   
   handleButtonEvent() {
     this.onSubmit();
   }
 
+  onCancel() {
+    this.employeeForm.reset();
+    this.submitButtonDisable = false;
+    this.updateButtonEnable = false;
+    this.cancelled.emit();
+  }
+
   updateFormWithEmployeeData(selectedEmployee: EmployeeData) {
+    this.submitButtonDisable = true;
+    this.updateButtonEnable = true;
+    
     this.employeeToEdit 
     this.employee = {... selectedEmployee};
     console.log(selectedEmployee)
@@ -89,13 +85,14 @@ export class EmployeeForm implements OnInit, OnChanges{
   }
 
   ngOnInit(): void {
-    this.employeeForm.get("name")?.valueChanges.subscribe((nameValue) => {console.log(nameValue)});
+    //this.employeeForm.get("name")?.valueChanges.subscribe((nameValue) => {console.log(nameValue)});
     //this.employeeSub = this.employeeDataService.getEmployeeUpdateData().subscribe((selectedEmployee: EmployeeData | null) => {
     //});
   }
+
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['employeeToEdit'] && this.employeeToEdit) {
     this.updateFormWithEmployeeData(this.employeeToEdit);
-  }
-}
+    }
+  } 
 }
