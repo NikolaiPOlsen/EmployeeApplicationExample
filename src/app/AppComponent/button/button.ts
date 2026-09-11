14cf022bc00c719d04d94feaa6c123e1605957f4
Nikolai <nikolai.pedersen.olsen@worksoft.no>
2026-09-11 11:02:51 +0200

    Removed non-working unnecessary code

--- src/app/AppComponent/button/button.ts
+++ src/app/AppComponent/button/button.ts
@@ -8,13 +8,16 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class Button {
   @Input() buttonText: string = 'Hello';
-  @Input() bgColor: string = 'var(--color-nav)'
-  @Input() colorText: string = 'var(--color-nav-text)'
+  @Input() bgColor: string = 'var(--color-nav)';
+  @Input() colorText: string = 'var(--color-nav-text)';
+  @Input() buttonState: boolean = false;
 
   @Output()
     buttonPressed = new EventEmitter();
+
     handleClick() {
-      this.buttonPressed.emit();
-      console.log("Button clicked")
+      console.log(this.buttonState)
+        this.buttonPressed.emit();
+        console.log("Button clicked")
     }
 }
