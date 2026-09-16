97eea2876d7687f6eb07a624cdaf37a18479f01a
Nikolai <nikolai.pedersen.olsen@worksoft.no>
2026-09-16 12:57:19 +0200

    Refactored DetailedPosts component: add IconButton import, output event for editing, and update navigation methods

--- src/app/Pages/posts/detailed-posts/detailed-posts.ts
+++ src/app/Pages/posts/detailed-posts/detailed-posts.ts
@@ -1,19 +1,23 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, EventEmitter, OnInit, Output, inject } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ApiService } from '../services/api-service';
 import { postsInterface } from '../services/posts-interface';
 import { commentsInterface } from '../services/comments-interface';
+import { IconButton } from '../../../AppComponent/icon-button/icon-button';
 
 @Component({
   selector: 'app-detailed-posts',
-  imports: [],
+  imports: [IconButton],
   templateUrl: './detailed-posts.html',
   styleUrl: './detailed-posts.scss',
 })
-export class DetailedPosts implements OnInit, OnDestroy{
+export class DetailedPosts implements OnInit {
+
+  @Output() idForEdit = new EventEmitter();
 
     post: postsInterface | null = null;
     comments: commentsInterface[] = [];
+    private router = inject(Router);
 
   constructor (
     private api: ApiService,
@@ -29,15 +33,21 @@ export class DetailedPosts implements OnInit, OnDestroy{
     });
   }
 
+  handleUpdate(id: string) {
+    this.idForEdit.emit(id)
+    this.router.navigate(['posts/edit-post/' + id])
+  }
+
+  handleDelete(id: string) {
+    this.api.deletePost(id).subscribe({
+      next: () => this.router.navigate(['/posts'])
+    })
+  }
+
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
     if (id) {
       this.loadSinglePost(id);
     }
   }
-
-  ngOnDestroy(): void {
-    
-  }
-
 }
