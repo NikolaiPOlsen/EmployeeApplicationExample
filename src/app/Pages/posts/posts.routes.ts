ac90b91fde6635bcac267f09cc1e324a8c43628c
Nikolai <nikolai.pedersen.olsen@worksoft.no>
2026-09-16 12:54:00 +0200

    added new route paths

--- src/app/Pages/posts/posts.routes.ts
+++ src/app/Pages/posts/posts.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { Posts } from './posts';
 import { DetailedPosts } from './detailed-posts/detailed-posts';
+import { NewPost } from './new-post/new-post';
+import { EditPost } from './edit-post/edit-post';
 
 export const postsRoutes: Routes = [
   { path: '', component: Posts },
+  { path: 'new-post', component: NewPost },
+  { path: 'edit-post/:id', component: EditPost },
   { path: ':id', component: DetailedPosts },
 ];
 
