df1cd644aafaafb7cda6ce37929aef93dbd7ed50
Nikolai <nikolai.pedersen.olsen@worksoft.no>
2026-09-17 09:55:55 +0200

    Refactored to include error handeling on api calls

--- src/app/Pages/posts/services/api-service.ts
+++ src/app/Pages/posts/services/api-service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { postsInterface } from './posts-interface';
 import { commentsInterface } from './comments-interface';
+import { catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -13,27 +14,50 @@ export class ApiService {
   constructor(private http: HttpClient) {}
 
   getPosts() {
-    return this.http.get<postsInterface[]>(this.apiUrl + 'posts');
+    return this.http.get<postsInterface[]>(this.apiUrl + 'posts').pipe(
+    catchError(( err: HttpErrorResponse ) => {
+      return throwError(() => err);
+    })
+  );
   }
 
   getSinglePost(id: string) {
-    return this.http.get<postsInterface>(this.apiUrl + 'posts/' + id);
+    return this.http.get<postsInterface>(this.apiUrl + 'posts/' + id).pipe(
+    catchError(( err: HttpErrorResponse ) => {
+      return throwError(() => err);
+    })
+  );
   }
 
   getSinglePostComments(id: string) {
-    return this.http.get<commentsInterface[]>(this.apiUrl + 'posts/' + id + '/comments');
+    return this.http.get<commentsInterface[]>(this.apiUrl + 'posts/' + id + '/comments').pipe(
+    catchError(( err: HttpErrorResponse ) => {
+      return throwError(() => err);
+    })
+  );
   }
 
   newPost(post: Pick<postsInterface, 'title' | 'body'>) {
-    return this.http.post<postsInterface>(this.apiUrl + 'posts', post);
+    return this.http.post<postsInterface>(this.apiUrl + 'posts', post).pipe(
+    catchError(( err: HttpErrorResponse ) => {
+      return throwError(() => err);
+    })
+  );
   }
 
   updatePost(post: Pick<postsInterface, 'title' | 'body'>, id: string) {
-    return this.http.put<postsInterface>(this.apiUrl + 'posts/' + id, post);
+    return this.http.put<postsInterface>(this.apiUrl + 'posts/' + id, post).pipe(
+    catchError(( err: HttpErrorResponse ) => {
+      return throwError(() => err);
+    })
+  );
   }
 
   deletePost(id: string) {
-    console.log("Running deletePost with id:", id);
-    return this.http.delete(this.apiUrl + 'posts/' + id);
+    return this.http.delete(this.apiUrl + 'posts/' + id).pipe(
+    catchError(( err: HttpErrorResponse ) => {
+      return throwError(() => err);
+    })
+  );
   }
 }
