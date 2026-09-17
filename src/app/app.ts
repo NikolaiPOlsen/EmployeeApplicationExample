40eb0375255dae16f737b9bbae9e5eb1c32ffecd
Nikolai <nikolai.pedersen.olsen@worksoft.no>
2026-09-17 14:14:23 +0200

    Refactored to include EmployeePageModule

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './app-components/navbar/navbar';
 import { PopUp } from './app-components/pop-up/pop-up';
+import { EmployeePageModule } from './pages/employee-page/employee-page.module';
 
 @Component({
-  imports: [RouterOutlet, Navbar, PopUp],
+  imports: [RouterOutlet, Navbar, PopUp, EmployeePageModule],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
