8f659ad5fad01a7fc08856800681d8e384cd6073
Nikolai <nikolai.pedersen.olsen@worksoft.no>
2026-09-17 09:53:45 +0200

    Included import for global popup component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './AppComponent/navbar/navbar';
+import { PopUp } from './AppComponent/pop-up/pop-up';
 
 @Component({
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, PopUp],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
